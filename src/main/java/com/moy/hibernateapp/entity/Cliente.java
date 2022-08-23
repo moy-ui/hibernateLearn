e72f0b4b599541c07710ac2d7b1f9193b546bb10
moy-ui <juancor830@gmial.com>
2022-08-23 17:00:29 -0500

    se inicio una aplicacion en java y se configuro el pom.xml para trabajar con hibernate

---
+++ src/main/java/com/moy/hibernateapp/entity/Cliente.java
@@ -0,0 +1,63 @@
+package com.moy.hibernateapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+
+
+    @Column(name = "forma_pago")
+    private String formaPago;
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", formaPago='" + formaPago;
+    }
+}
