0192ab35f8e698d5bd38f86411c7ce276c3770d1
moy-ui <juancor830@gmial.com>
2022-08-24 12:51:22 -0500

    Se agrego una clase ejemplo para actualizar los datos

---
+++ src/main/java/com/moy/hibernateapp/HibernateEditar.java
@@ -0,0 +1,36 @@
+package com.moy.hibernateapp;
+
+import com.moy.hibernateapp.entity.Cliente;
+import com.moy.hibernateapp.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+import javax.swing.*;
+
+public class HibernateEditar {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+        try{
+            Long id = Long.valueOf(JOptionPane.showInputDialog("Ingrese el id del cliente a modificar"));
+            Cliente c = em.find(Cliente.class, id);
+
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre: ", c.getNombre() );
+            String apellido = JOptionPane.showInputDialog("Ingrese el apellido: ", c.getApellido() );
+            String pago = JOptionPane.showInputDialog("Ingrese la forma de pago: ", c.getFormaPago() );
+            em.getTransaction().begin();
+            c.setNombre(nombre);
+            c.setApellido(apellido);
+            c.setFormaPago(pago);
+            em.merge(c);
+
+
+            em.getTransaction().commit();
+            System.out.println(c);
+        }catch (Exception e){
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+    }
+}
